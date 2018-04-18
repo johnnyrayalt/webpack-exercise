6f58fe95265ea317f756bafb713edc864fa2c8e7
Brea Borlas and Johnny Ray Alt <johnny@>
2018-04-18 11:08:49 -0700

    Add front end

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-// import $ from 'jquery';
+import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Haiku } from "./../src/haiku.js";
+
+$(document).ready(function() {
+  $("#haiku-form").submit(function(event){
+    let line1 = $("#haiku-input-1").val();
+    let line2 = $("#haiku-input-2").val();
+    let line3 = $("#haiku-input-3").val();
+    let newHaiku = new Haiku(line1, line2, line3);
+    let result = newHaiku.isHaiku();
+    $("#result").text(result);
+    event.preventDefault();
+  })
+});
